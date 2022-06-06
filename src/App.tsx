ba0a0068c5b04219772b69cc7451c9f8b33ec16c
soiferj <soiferj@gmail.com>
2022-06-05 22:18:43 -0700

    Song is playing with button

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import { ImageCanvas } from "./components/ImageCanvas"
 
 function App() {
   const [largestStroke, setLargestStroke] = useState(4)
+  const [playAudio, setPlayAudio] = useState(false)
   useEffect(() => {
   }, [largestStroke])
+  useEffect(() => {
+  }, [playAudio])
   return (
     <div className="App">
       <input type="range" value={largestStroke} min={1} max={20} onChange={(e) => setLargestStroke(JSON.parse(e.currentTarget.value))} />
-      <ImageCanvas largestStroke={largestStroke} shrinkRate={6} />
+      <br />
+      <input type="button" value={playAudio ? "Stop" : "Play"} onClick={() => setPlayAudio(!playAudio)} />
+      <ImageCanvas largestStroke={largestStroke} shrinkRate={6} playAudio={playAudio} />
     </div>
   )
 }
--- src/components/ImageCanvas/ImageCanvas.tsx
+++ src/components/ImageCanvas/ImageCanvas.tsx
@@ -2,20 +2,27 @@ import React, { useCallback } from "react";
 import Sketch from "react-p5";
 import p5Types, { Image } from "p5"; //Import this for typechecking and intellisense
 import localImage from "../../assets/Bicoastal2.jpg"
+import localSong from "../../assets/Beat.wav"
 import { useDrawImage, useSetupImage } from "../../hooks";
+import "p5/lib/addons/p5.sound";
 
 interface Props {
     largestStroke: number
     shrinkRate: number
+    playAudio: boolean
 }
 
 let img: Image;
+let song: SoundFile;
 
 export const ImageCanvas = (props: Props): React.ReactElement => {
-    const { largestStroke, shrinkRate } = props;
+    const { largestStroke, shrinkRate, playAudio } = props;
     const key = JSON.stringify(props)
     const preload = (p5: p5Types) => {
         img = p5.loadImage(localImage)
+
+        p5.soundFormats("wav", "mp3")
+        song = p5.loadSound(localSong)
     }
 
     const { drawImage } = useDrawImage()
@@ -33,6 +40,13 @@ export const ImageCanvas = (props: Props): React.ReactElement => {
         const getPixelValue = (col: number, row: number) => {
             const originalPixel = img.get(col, row);
             return [originalPixel[0], originalPixel[1], originalPixel[2], originalPixel[3]]; // r,g,b,a
+        }
+
+        if (playAudio && !song.isPlaying()) {
+            song.play()
+        }
+        else if (!playAudio) {
+            song.stop()
         }
 
         drawImage(p5, img, {
